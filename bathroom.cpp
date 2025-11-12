cd76ad16b02b9de5e999bbb587c40270b06a095e
jrholt04 <jrholt04@gmail.com>
2025-11-12 16:13:26 -0500

    another ver

--- bathroom.cpp
+++ bathroom.cpp
@@ -14,7 +14,6 @@ using namespace std;
 
 int main(int argc, char **argv) {
     bathMax = 4;
-    bathroom = createBathroom(bathroom, bathMax);
 
     pthread_t elfThread, dwarfThread;
     pthread_attr_t attr;
@@ -23,6 +22,7 @@ int main(int argc, char **argv) {
     sem_init(&bath_mutex, 0, 1);
     sem_init(&dwarf_mutex, 0, 1);
     sem_init(&elf_mutex, 0, 1);
+    sem_init(&printSem, 0, 1);
 
     //create multiple elves and multiple dwarves 
     pthread_create(&elfThread, &attr, elves, NULL);
@@ -34,81 +34,70 @@ int main(int argc, char **argv) {
     
 }
 
-Bathroom createBathroom(Bathroom bathroom, int bathMax){
-    bathroom.currentSpecies = "NONE";
-    bathroom.countInBath = 0;
-    bathroom.bathMax = bathMax;
-    return bathroom;
-}
-
 void *elves(void *param){
     while (true) {
-        sem_wait(&bath_mutex);
-        sem_wait(&dwarf_mutex);
+        waitingElves++;
+        sem_wait(&elf_mutex);
+        if (!(countInBath == bathMax)){
+            sem_wait(&bath_mutex);
+            waitingElves--;
+            countInBath++; //if room enter bathroom 
+        }
+        sem_post(&elf_mutex);
 
-        if (bathroom.countInBath == bathMax){
-            waitingElves++;
-        }
-        else {
-            bathroom.countInBath++;
-            printf("one elf entered %d ELFs in bathroom\n", bathroom.countInBath);
-        }
-
-        if (bathroom.countInBath == 1){
-            sem_wait(&elf_mutex); //lock out dwarves
-        }
-    
+        sem_wait(&printSem);
+        printf("one elf entered %d elfs in bathroom waiting dwarfs: %d\n", countInBath, waitingDwarves);
+        sem_post(&printSem);
         sleep(2);
-    
-        bathroom.countInBath--; 
-        printf("one elf left %d ELFs in bathroom\n", bathroom.countInBath);
-        //kick all the elves out if there is waiting dwarves
-        if (waitingDwarves > 0){
-            bathroom.countInBath = 0;
-            printf("all elfs left becasue there are dwarfs waiting count: %d\n", bathroom.countInBath);
-        }
+
+        sem_wait(&elf_mutex);
+        countInBath--;
         
-        printf("one elf left %d ELFs in bathroom\n", bathroom.countInBath);
-        if (bathroom.countInBath == 0){
-            sem_post(&elf_mutex);
-            sem_post(&dwarf_mutex);
+        sem_wait(&printSem);
+        printf("one elf entered %d elfs in bathroom waiting dwarfs: %d\n", countInBath, waitingDwarves);
+        sem_post(&printSem);
+
+        if (waitingDwarves > 0){
+            countInBath = 0;
+            sem_post(&bath_mutex);
         }
-        sem_post(&bath_mutex);
+        else if (countInBath == 0){
+            sem_post(&bath_mutex);
+        }
+        sem_post(&elf_mutex);
     }
 }
+
 void *dwarves(void *param){
     while (true) {
-        sem_wait(&bath_mutex);
-        sem_wait(&elf_mutex);
-
-        if (bathroom.countInBath == bathMax){
-            waitingDwarves++;
+        waitingDwarves++;
+        sem_wait(&dwarf_mutex);
+        if (!(countInBath == bathMax)){
+            sem_wait(&bath_mutex);
+            waitingDwarves--;
+            countInBath++; //if room enter bathroom 
         }
-        else {
-            bathroom.countInBath++;
-            printf("one dwarf entered %d DWARFs in bathroom\n", bathroom.countInBath);
-        }
+        sem_post(&dwarf_mutex);
 
-        if (bathroom.countInBath == 1){
-            sem_wait(&dwarf_mutex); //lock out elves
-        }
-
-        
+        sem_wait(&printSem);
+        printf("one dwarf entered %d elfs in bathroom waiting elf: %d\n", countInBath, waitingElves);
+        sem_post(&printSem);
         sleep(2);
-        
-        bathroom.countInBath--; 
-        printf("one dwarf left %d DWARFs left \n", bathroom.countInBath);
-        //kick all the dwarfs out if there is waiting elfs
-        if (waitingElves > 0){
-            bathroom.countInBath = 0;
-            printf("all dwarfs left becasue there are elfs waiting count: %d\n", bathroom.countInBath);
-        }
-        
-        if (bathroom.countInBath == 0){
-            sem_post(&dwarf_mutex);
-            sem_post(&elf_mutex);
-        }
-        sem_post(&bath_mutex);
 
+        sem_wait(&dwarf_mutex);
+        countInBath--;
+        
+        sem_wait(&printSem);
+        printf("one elf entered %d dwarfs in bathroom waiting elf: %d\n", countInBath, waitingElves);
+        sem_post(&printSem);
+
+        if (waitingElves > 0){
+            countInBath = 0;
+            sem_post(&bath_mutex);
+        }
+        else if (countInBath == 0){
+            sem_post(&bath_mutex);
+        }
+        sem_post(&dwarf_mutex);
     }
 }
--- bathroom.h
+++ bathroom.h
@@ -7,22 +7,14 @@
 
 using namespace std;
 
-struct Bathroom {
-    string currentSpecies;
-    int countInBath;
-    int bathMax;
-};
-
-
-Bathroom createBathroom(Bathroom bathroom, int bathMax);
-
 void *elves(void *param);
 void *dwarves(void *param);
 
-Bathroom bathroom;//at the start there are none of either of the elves waiting
+int countInBath = 0;
 int waitingElves = 0;
 int waitingDwarves = 0;
 int bathMax;
 sem_t dwarf_mutex;
 sem_t elf_mutex;
 sem_t bath_mutex;
+sem_t printSem;
